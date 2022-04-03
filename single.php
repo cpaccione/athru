d04ed63714170ec180a594f4077f55ece16aabfa
Chris Paccione <cpaccione77@gmail.com>
2022-04-03 00:02:17 -0500

    blog updates

--- single.php
+++ single.php
@@ -18,15 +18,15 @@ get_header();
 
 ?>
 
-<section class="blog-hero" style="background-image: url(<?php echo $feat_image; ?>); height: 350px; background-size: cover; background-repeat: no-repeat; background-position: center center;">
+<section class="blog-hero h-[50vh] bg-cover bg-no-repeat bg-center" style="background-image: url(<?php echo $feat_image; ?>);">
 
 </section>
 
-<div class="blog-wrap">
+<div class="blog-wrap bg-white">
     <div class="container">
-        <div id="primary" class="content-area">
+        <div id="primary" class="content-area grid px-4 gap-x-7 md:grid-cols-3">
 			
-			<main id="main" class="site-main">
+			<main id="main" class="site-main col-span-2">
 
 				<?php
 				while ( have_posts() ) :
@@ -35,19 +35,26 @@ get_header();
 
 					get_template_part( 'template-parts/content-single', get_post_type() );
 
+				?>
+				<div class="pb-12">
+				<?php
 					the_post_navigation(
 						array(
 							'prev_text' => '<span class="border-2 border-solid border-blue text-base text-blue text-center flex h-12 w-40 items-center justify-center rounded-sm text-cont-gray hover:bg-blue hover:text-white mr-6">' . esc_html__( 'PREVIOUS', 'realestate' ) . '</span>',
 							'next_text' => '<span class="border-2 border-solid border-blue text-base text-blue flex h-12 w-40 items-center justify-center rounded-sm text-cont-gray hover:bg-blue hover:text-white">' . esc_html__( 'NEXT', 'realestate' ) . '</span>',
 						)
 					);
-
+					?>
+				</div>
+				<?php
 				endwhile; // End of the loop.
 				?>
 
 			</main><!-- #main -->
 			
-			<?php get_sidebar(); ?>
+			<aside class="col-span-1 md:pt-12">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</aside>
 				
 		</div><!-- #primary -->
 	</div>
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,26 +9,26 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('py-12'); ?>>
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h1 class="entry-title pb-4">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h2 class="entry-title pb-4"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) :
 			?>
 		<?php endif; ?>
 
-		<div class="entry-meta">
+		<div class="entry-meta pb-4">
 			<?php athrupartners_posted_on('F, Y'); ?>
 			<?php athrupartners_posted_by(); ?>
 		</div>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content prose prose-base">
 
 			<?php
 			the_content();
@@ -41,7 +41,7 @@
 
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+	<footer class="entry-footer mt-12">
 		<?php athrupartners_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
